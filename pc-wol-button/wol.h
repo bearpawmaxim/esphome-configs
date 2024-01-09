c1f2fc9642cdcb3d2cc08a7ae16c4e21b38a43a8
root <root@homeassistant.local.hass.io>
2024-01-09 23:50:00 +0200

    config files backup on 2024-01-09

--- pc-wol-button/wol.h
+++ pc-wol-button/wol.h
@@ -8,16 +8,15 @@ WakeOnLan WOL(UDP);
 const char* TAG = "WOL";
 
 void wol_setup() {
-  WOL.setRepeat(10, 200);
+  WOL.setRepeat(3, 200);
   WOL.calculateBroadcastAddress(WiFi.localIP(), WiFi.subnetMask());
 }
 
 void wol_send(const char *mac_addr) {
   bool result = WOL.sendMagicPacket(mac_addr, 7);
   if (result) {
-    ESP_LOGI(TAG, "WOL Packet successfully sent.");
+    ESP_LOGI(TAG, "WOL Packet successfully sent to %s.", mac_addr);
   } else {
-    ESP_LOGE(TAG, "Error sending WOL Packet!");
+    ESP_LOGE(TAG, "Error sending WOL Packet to %s!", mac_addr);
   }
 }
-
