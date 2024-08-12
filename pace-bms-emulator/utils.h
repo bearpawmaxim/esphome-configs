7061e82df72886294ba3dafd1abbec009632e2c3
root <root@homeassistant.local.hass.io>
2024-08-12 23:50:00 +0300

    pace-bms-emulator: initial version ready.

--- pace-bms-emulator/utils.h
+++ pace-bms-emulator/utils.h
@@ -1,6 +1,8 @@
 #pragma once
 
-uint32_t get_ascii_char(const std::string &input, size_t offset) {
+using namespace esphome;
+
+uint32_t get_ascii_char_(const std::string &input, size_t offset) {
   uint32_t chr = static_cast<uint32_t>(input[offset]);
   return chr > 127 ? 0x5F : chr;
 }
@@ -11,7 +13,7 @@ template <typename T>
     uint8_t len = input.length();
     for (size_t i = 0; i < num_chars; ++i) {
       if (offset + i < len) {
-        data |= get_ascii_char(input, offset + i) << (8 * (num_chars - 1 - i));
+        data |= get_ascii_char_(input, offset + i) << (8 * (num_chars - 1 - i));
       }
     }
     return data;
@@ -25,4 +27,16 @@ uint16_t pack_chars16(const std::string &input, size_t offset) {
 // Packs 4 characters into a 32-bit integer
 uint32_t pack_chars32(const std::string &input, size_t offset) {
   return pack_chars_impl<uint32_t>(input, offset, 4);
+}
+
+// Packs 16 switch values into a 16-bit integer
+uint16_t pack_flags16(const Switch* flags[], size_t num_flags) {
+  uint16_t result = 0;
+  for (int i = 0; i < 16; i++) {
+    if (i >= num_flags) {
+      return result;
+    }
+    result |= (flags[i]->state ? 1 : 0) << i;
+  }
+  return result;
 }
