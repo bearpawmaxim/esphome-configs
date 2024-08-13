50f3ff267d63eb8d6a8380169be55e24ca26a9a6
root <root@homeassistant.local.hass.io>
2024-08-13 23:50:00 +0300

    pace-bms-emulator: modify pack_flags16 method so that it accepts the array of booleans instead the array of Switches.

--- pace-bms-emulator/utils.h
+++ pace-bms-emulator/utils.h
@@ -1,7 +1,5 @@
 #pragma once
 
-using namespace esphome;
-
 uint32_t get_ascii_char_(const std::string &input, size_t offset) {
   uint32_t chr = static_cast<uint32_t>(input[offset]);
   return chr > 127 ? 0x5F : chr;
@@ -30,13 +28,13 @@ uint32_t pack_chars32(const std::string &input, size_t offset) {
 }
 
 // Packs 16 switch values into a 16-bit integer
-uint16_t pack_flags16(const Switch* flags[], size_t num_flags) {
+uint16_t pack_flags16(const bool flags[], size_t num_flags) {
   uint16_t result = 0;
   for (int i = 0; i < 16; i++) {
     if (i >= num_flags) {
       return result;
     }
-    result |= (flags[i]->state ? 1 : 0) << i;
+    result |= (flags[i] ? 1 : 0) << i;
   }
   return result;
 }
